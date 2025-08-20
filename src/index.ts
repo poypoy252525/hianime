a7c1501894d14d2b03296bc809a990a7077736d8
delfin <carljefferson.delfin@gmail.com>
2025-08-20 16:08:01 +0800

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import Hianime from "./hianime";
-
-export default Hianime;
-export { Hianime };
+export { default as Hianime } from "./hianime";
 
 export type { HianimeResult, Result, Language } from "./types/hianime-result";
 export { Type } from "./types/hianime-result";
 export type { HianimeEpisode as Episode } from "./types/hianime-episode";
+export type { HianimeSources } from "./types/hianime-sources";
